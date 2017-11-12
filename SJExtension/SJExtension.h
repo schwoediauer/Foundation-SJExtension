8578d0b973abd677555baa3b94b2498ae4f5300a
Juergen Schwoediauer <juergen@imac.local>
2017-11-12 19:25:43 +0100

    Initial Commit

---
+++ SJExtension/SJExtension.h
@@ -0,0 +1,19 @@
+//
+//  SJExtension.h
+//  SJExtension
+//
+//  Created by Juergen Schwoediauer on 12.11.17.
+//  Copyright © 2017 Juergen Schwoediauer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SJExtension.
+FOUNDATION_EXPORT double SJExtensionVersionNumber;
+
+//! Project version string for SJExtension.
+FOUNDATION_EXPORT const unsigned char SJExtensionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SJExtension/PublicHeader.h>
+
+
